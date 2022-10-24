802a1b4823dea8c7c62bdbcabe8ac70435df416c
Satont <satontworldwide@gmail.com>
2022-10-24 18:09:46 +0300

    feat(api): use default `0.0.0.0` host for listening

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { cleanEnv, num, str } from 'envalid'
 
 export const env = cleanEnv(process.env, {
-  HOST: str({ default: 'localhost' }),
+  HOST: str({ default: '0.0.0.0' }),
   PORT: num({ default: 5005 }),
   API_TOKEN: str(),
   PACK_DESCRIPTION: str({ default: 'Chat Emotes' })
